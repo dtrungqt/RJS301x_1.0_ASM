9fb724844b22d5449099b69b75fbd8294c4a317a
DTrung <ngdinhtrung.qt@gmail.com>
2022-08-30 18:11:21 +0700

    7th: search movie

--- src/UI/ResultList.js
+++ src/UI/ResultList.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ResultList = () => {
-  return <div></div>;
+  return (
+    <div className="resultList-container">
+      <h1 className="resultList-title">Search Result</h1>
+    </div>
+  );
 };
 
 export default ResultList;
